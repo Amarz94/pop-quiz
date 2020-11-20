0a76e223e3d787281d85b8c70fe7345d28e7d501
Alex Marzullo <marzullo94@gmail.com>
2020-11-20 17:58:18 -0500

    Added code to the Submit page so it would store the input

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ window.onload = function() {
     var liAnswer    = document.querySelector(".liAnswer")
     var eval        = document.querySelector(".eval")
     var curScore    = document.querySelector(".score")
+    var scoreCard   = document.querySelector("#scoreCard")
+    var initInput   = document.querySelector("#initInput")
+    var submitBtn   = document.querySelector(".submitBtn")
+    var msgDiv      = document.querySelector("#msg");
 
     var score       = 0
     var secondsLeft = 60
@@ -78,32 +82,44 @@ window.onload = function() {
     liAnswer.addEventListener("click", function(event) {
         if(answer === event.target.textContent) {
             eval.textContent = "Correct!"
-            score = score + 5
+            score = score + 10
             curScore.innerHTML = "Score: " + score
 
         } else {
             console.log(event.target.textContent)
             eval.textContent = "Wrong!"
             secondsLeft = secondsLeft -10
+            score = score - 5
             
         }
         questStage();
     })
         
-
-        
-        
-        
-
-
-
-       
-
-    
-    
-
     function endPage() {
+        curScore.innerHTML = ""
         hsWin.setAttribute("style", "display: block")
         quizWin.setAttribute("style", "display: none")
+        if(score > 0) {
+            scoreCard.setAttribute("value", score + secondsLeft)
+        } else {
+            scoreCard.setAttribute("value", score - score)
+        }
+
     }
+
+
+    submitBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var initial = document.querySelector("#initInput").value;
+        console.log(initial)
+
+        if(initial === ""){
+            msgDiv.textContent = "Error: Cannot leave initals blank!";
+        } else {
+            msgDiv.textContent = "Success: Registered successfully!";
+            localStorage.setItem("Score", score);
+            localStorage.setItem("Initials", initial);
+        }
+    })
 }
